fdc05037e0257c9944c3a127aec2e91adb0fdb9c
reireynoso <reireynoso@gmail.com>
2020-04-06 12:28:42 -0400

    updated routes and removed enrollments view directory

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'courses/index'
-  get 'students/index'
-  get 'students/new'
-  get 'students/create'
-  get 'students/show'
-  get 'students/edit'
-  get 'students/update'
-  get 'students/destroy'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "/students", to: "students#index", as: "students" #index
+
 end
